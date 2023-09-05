2163e87631e8dd16d4aa8e9324dfe8ab173e3c8b
Chris Koning <chris@autocloud.dev>
2023-09-05 14:50:49 -0700

    feat: add provider config and version constraints

---
+++ versions.tf
@@ -0,0 +1,11 @@
+
+terraform {
+  required_version = ">= 1.0, < 1.6.0"
+
+  required_providers {
+    autocloud = {
+      source  = "autoclouddev/autocloud"
+      version = "0.13.0"
+    }
+  }
+}
